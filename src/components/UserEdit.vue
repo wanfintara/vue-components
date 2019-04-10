f4e69baf075016054e6242ffee9105a71f1a761a
wanfintara <wanfintara@gmail.com>
2019-04-10 23:42:53 +0700

    Eventbus add function

--- src/components/UserEdit.vue
+++ src/components/UserEdit.vue
@@ -16,7 +16,8 @@ export default{
         editAge(){
             this.userAge = 30;
             // this.$emit('ageWasEdited', this.userAge);
-            eventBus.$emit('ageWasEdited', this.userAge);
+            // eventBus.$emit('ageWasEdited', this.userAge);
+            eventBus.changeAge(this.userAge);
         }
     }
 }
